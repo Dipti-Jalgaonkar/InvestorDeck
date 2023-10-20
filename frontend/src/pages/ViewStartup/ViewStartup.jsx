cb49d9560204f1c23b0cb7858703e3d22e445e74
Gautami06 <gautamigupta0620@gmail.com>
2023-10-20 23:55:55 +0530

    startup page changes

--- frontend/src/pages/ViewStartup/ViewStartup.jsx
+++ frontend/src/pages/ViewStartup/ViewStartup.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import './ViewStartup.css'
 import { Grid, GridItem } from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, CardFooter,Image, Stack, Heading, Text, Button } from '@chakra-ui/react'
 function ViewStartup() {
   return (
-    <Grid
+    <div className="view_container">
+      <h2 className='view_h1'>Startup looking for investments : </h2>
+      <Grid
   h='200px'
   templateRows='repeat(2, 1fr)'
   templateColumns='repeat(6, 1fr)'
   gap={4}
 >
-  <GridItem colSpan={2} bg='papayawhip' />
-  <GridItem colSpan={2} bg='papayawhip' />
-  <GridItem colSpan={2} bg='papayawhip' />
+  <GridItem colSpan={2} bg='papayawhip' >
+  </GridItem>
+  <GridItem colSpan={2} bg='papayawhip'></GridItem>
+  <GridItem colSpan={2} bg='papayawhip'></GridItem>
 </Grid>
+    </div>
   )
 }
 
